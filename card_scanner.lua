4513e2de54e89a5357c3ea4b512f77e281223055
Buckaroo Banzai <39065740+BuckarooBanzay@users.noreply.github.com>
2020-11-23 20:38:54 +0100

    add same plane mesecon rules

--- card_scanner.lua
+++ card_scanner.lua
@@ -4,10 +4,16 @@ local get_switch_rules = function(param2)
 
 	-- param2 = 2
 	local rules = {
+		-- lower plane
 		{x=1, y=-1, z=-1},
 		{x=1, y=-1, z=1},
 		{x=0, y=-1, z=-1},
 		{x=0, y=-1, z=1},
+		-- same plane
+		{x=1, y=0, z=0},
+		{x=-1, y=0, z=0},
+		{x=0, y=0, z=1},
+		{x=0, y=0, z=-1},
 	}
 
 -- Left and right when looking to +y ?
